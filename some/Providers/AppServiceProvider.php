70878650fc6c2ede0c0bbca2219489f8851594e2
Westwind Galeaf <galeaf@kodeks.ru>
2017-05-29 14:01:17 +0300

    CRT: добавлена структура для приложения some

---
+++ some/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Some\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SomeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
